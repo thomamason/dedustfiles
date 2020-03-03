228d4eab4d4ccf611d296122e8ab8ac92e906461
thomamason <thomas.g.featherstone@hotmail.com>
2020-03-03 12:43:33 +0000

    dwm scripts

--- .local/bin/bar-functions/dwm_date.sh
+++ .local/bin/bar-functions/dwm_date.sh
@@ -8,9 +8,9 @@
 dwm_date () {
     printf "%s" "$SEP1"
     if [ "$IDENTIFIER" = "unicode" ]; then
-        printf "📆 %s" "$(date "+%a %d-%m-%y %T")"
+        printf "%s" "$(date "+%a %d-%m-%y %T")"
     else
-        printf "DAT %s" "$(date "+%a %d-%m-%y %T")"
+        printf "%s" "$(date "+%a %d-%m-%y %T")"
     fi
     printf "%s\n" "$SEP2"
 }
